d695e004e4b9cb739bf04c939a057605e1dda18c
nyel-101 <dandevjr@gmail.com>
2025-02-16 20:22:05 +0000

    chore(config): refactor auth tokens

--- furry/src/components/helper.py
+++ furry/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_27951b6245da4b38985f3b9f40c39cf4 = () => {
-  return '2026-06-14T17:04:49Z';
+const node_32cefc121f414cfbb6b9bd33906eda13 = () => {
+  return '2025-02-16T20:22:05Z';
 };
