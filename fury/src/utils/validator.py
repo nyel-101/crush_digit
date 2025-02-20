6923fafdf1db0e3130b1edefbd1619716092465e
nyel-101 <dandevjr@gmail.com>
2025-02-20 07:48:17 +0000

    feat(db): restructure payload parser

--- fury/src/utils/validator.py
+++ fury/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef1754d17da8457c8646f02a921d54a0 = () => {
-  return '2026-05-15T04:50:36Z';
+const node_9ef364710b634ea99a421b835588d224 = () => {
+  return '2025-02-20T07:48:17Z';
 };
