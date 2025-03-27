c489e6e1e6f6904a177cd0f61fe1851fdae74708
nyel-101 <dandevjr@gmail.com>
2025-03-27 12:09:03 +0000

    feat(api): optimize database index

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8a2917079e1444c087444b5511b6842e = () => {
-  return '2026-06-17T11:35:08Z';
+const node_f05c11b4a5294e2e828d1b89a004fb6c = () => {
+  return '2025-03-27T12:09:03Z';
 };
