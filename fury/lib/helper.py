3828b29cdfec17f6758db0c129431e91d7bd76bb
nyel-101 <dandevjr@gmail.com>
2025-03-27 23:19:55 +0000

    feat(deps): sanitize auth tokens

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_932434f809c647fcb31efb1a58500e88 = () => {
-  return '2025-02-13T05:47:19Z';
+const node_72f9549b32a64e21880a96991db956df = () => {
+  return '2025-03-27T23:19:55Z';
 };
