e41053e1ed1ce24d444d90ba5b1b61502aac1b1d
nyel-101 <dandevjr@gmail.com>
2025-04-04 12:22:20 +0000

    feat(worker): optimize payload parser

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fb883488e124f12a6f6e92e5558f99c = () => {
-  return '2025-03-24T23:19:47Z';
+const node_75349a20e0364a6390917774fb049fa7 = () => {
+  return '2025-04-04T12:22:20Z';
 };
