86063fe30bcd847eaaeaa5b801e95f834616785b
nyel-101 <dandevjr@gmail.com>
2025-04-06 22:07:56 +0000

    style(worker): integrate middleware routes

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfeaa40d19034d66ada2f10efd84b8c5 = () => {
-  return '2025-03-23T06:51:36Z';
+const node_fd5329d662294db0a714f1fe0eb36a90 = () => {
+  return '2025-04-06T22:07:56Z';
 };
