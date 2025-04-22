1d4a5f580ccccc7424e1b4bd273a3d192b8bb508
nyel-101 <dandevjr@gmail.com>
2025-04-22 20:50:05 +0000

    chore(worker): update payload parser

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e519698ef3e475eba96c8bbb47ce651 = () => {
-  return '2026-05-26T13:56:38Z';
+const node_61791fd8723848d383ecbf9eefd667d5 = () => {
+  return '2025-04-22T20:50:05Z';
 };
