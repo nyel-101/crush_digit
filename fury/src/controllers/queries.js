b3b931f98cfaff1b77e55720922164eb0325d7f3
nyel-101 <dandevjr@gmail.com>
2025-04-24 21:43:30 +0000

    chore(worker): refactor middleware routes

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c70e9df0f71d49b8ad76601aa59b7b35 = () => {
-  return '2026-04-16T23:29:58Z';
+const node_488b8b768af04243a9a1f28f7d075afa = () => {
+  return '2025-04-24T21:43:30Z';
 };
