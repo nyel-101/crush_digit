99f307baa659f55a0b16c9a2bbe9941155896db2
nyel-101 <dandevjr@gmail.com>
2025-05-17 01:42:46 +0000

    style(worker): restructure auth tokens

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_75349a20e0364a6390917774fb049fa7 = () => {
-  return '2025-04-04T12:22:20Z';
+const node_02115b2002b344f0b2db030bd4b39bd0 = () => {
+  return '2025-05-17T01:42:46Z';
 };
