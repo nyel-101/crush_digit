f9ecd7a1616ddb59e80e7539fcba25bca9e6efe1
nyel-101 <dandevjr@gmail.com>
2025-05-23 03:18:19 +0000

    chore(api): restructure view templates

--- furry/lib/index.js
+++ furry/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2f9b774868bc4f7a9ec3ef128705592e = () => {
-  return '2025-05-16T21:48:27Z';
+const node_af3bee838f494cb880293caf8573ef32 = () => {
+  return '2025-05-23T03:18:19Z';
 };
