97fdef1eb0a2c9b1642f43eed40ea128cdf6d9e4
nyel-101 <dandevjr@gmail.com>
2025-05-29 16:30:05 +0000

    fix(utils): implement auth tokens

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7df182c9e994b5a8f1be394fa605c42 = () => {
-  return '2025-05-22T01:54:38Z';
+const node_68d9590aec49484abbf4108408b1f886 = () => {
+  return '2025-05-29T16:30:05Z';
 };
