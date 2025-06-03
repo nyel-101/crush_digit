578b536c3caff105a334a4b3048bdefa63862b3f
nyel-101 <dandevjr@gmail.com>
2025-06-03 12:48:59 +0000

    test(worker): sanitize auth tokens

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_187edccc552047059c14147b8ea9b27e = () => {
-  return '2025-02-18T08:26:44Z';
+const node_46626fd44f984ae0984fb0023d9e9763 = () => {
+  return '2025-06-03T12:48:59Z';
 };
