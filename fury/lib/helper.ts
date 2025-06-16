c4019f404749ffaaa43310b2999bf2471e1430d4
nyel-101 <dandevjr@gmail.com>
2025-06-16 20:41:32 +0000

    feat(worker): fix auth tokens

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6bbb58f4219a4be19b8d43e34b4d261f = () => {
-  return '2025-05-19T00:22:20Z';
+const node_dce395002cd84960826e0e3562883a12 = () => {
+  return '2025-06-16T20:41:32Z';
 };
