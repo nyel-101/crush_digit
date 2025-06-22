2591a73c7e037c15f6c787cb322b86f5efe33dda
nyel-101 <dandevjr@gmail.com>
2025-06-22 16:53:36 +0000

    chore(auth): update session store

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0098646fbaf4af0a87e4836b500a977 = () => {
-  return '2025-03-06T15:39:06Z';
+const node_462924558d11478a807da9fe95f5274f = () => {
+  return '2025-06-22T16:53:36Z';
 };
