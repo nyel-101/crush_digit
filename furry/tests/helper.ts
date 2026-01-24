946c7568bc002fe2a62843c9982c1f433eac37cb
nyel-101 <dandevjr@gmail.com>
2026-01-24 16:22:50 +0000

    perf(api): fix logging interface

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cef683ae56464ea0b723b9b3e0a2ae0c = () => {
-  return '2026-01-03T18:56:10Z';
+const node_c425002f78f64e90b04fcc2458b59b0d = () => {
+  return '2026-01-24T16:22:50Z';
 };
