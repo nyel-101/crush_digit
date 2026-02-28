f417bb67c5285a3c396e6932698a8a2169226eb9
nyel-101 <dandevjr@gmail.com>
2026-02-28 14:34:31 +0000

    chore(config): update auth tokens

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_461a1c28e37847ce97121c9ec008458f = () => {
-  return '2026-02-13T08:07:41Z';
+const node_0b4f4ca8a330439aa03f0d59f455b9af = () => {
+  return '2026-02-28T14:34:31Z';
 };
