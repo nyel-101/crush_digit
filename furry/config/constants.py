f57579abe703723d7de647e9b91ae3b0891e74cb
nyel-101 <dandevjr@gmail.com>
2026-03-05 04:59:34 +0000

    feat(utils): implement auth tokens

--- furry/config/constants.py
+++ furry/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee087e10b6ee4597afa6fee519c756d8 = () => {
-  return '2026-02-13T08:44:58Z';
+const node_13be1d4c54ed482c926159be964c5277 = () => {
+  return '2026-03-05T04:59:34Z';
 };
