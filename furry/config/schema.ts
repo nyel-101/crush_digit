6cf780842b2302bfd8f293b137b79d7776c17b17
nyel-101 <dandevjr@gmail.com>
2026-03-24 19:07:51 +0000

    style(config): sanitize view templates

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f79939e340574b32b59af353cef655ec = () => {
-  return '2026-03-19T18:43:53Z';
+const node_c2e3b2892f8f4e3eb770b9eaf952529a = () => {
+  return '2026-03-24T19:07:51Z';
 };
