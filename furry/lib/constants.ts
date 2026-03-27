20da7ebab812c0e5929c0c58cee7793f83d8d074
nyel-101 <dandevjr@gmail.com>
2026-03-27 08:39:13 +0000

    feat(auth): refactor payload parser

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec41a0855ca04f3db68cba2bb435438b = () => {
-  return '2026-01-07T21:14:56Z';
+const node_a6a4a3780c3b4039a0e95db21940a11e = () => {
+  return '2026-03-27T08:39:13Z';
 };
