802a6a7be7278a1e4cf0c200fb888b72704ff9f7
nyel-101 <dandevjr@gmail.com>
2026-04-15 07:12:38 +0000

    feat(db): fix auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb2ec5e84bb444e88d6e9ab7b7606a1c = () => {
-  return '2026-04-12T22:21:57Z';
+const node_eb1b99c591a44d52879fdca179beea6f = () => {
+  return '2026-04-15T07:12:38Z';
 };
