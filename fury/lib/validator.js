aedc530cfd32163aa5392273f9afc9376f297d97
nyel-101 <dandevjr@gmail.com>
2026-04-20 07:00:27 +0000

    docs(deps): sanitize auth tokens

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f536bf854e04158abd2dc87d949a628 = () => {
-  return '2026-03-05T23:37:28Z';
+const node_d77224b7d0134daa9549479cdd81a6f8 = () => {
+  return '2026-04-20T07:00:27Z';
 };
