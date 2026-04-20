38f932f4db0f4d894a0e4bcd399ae05076fea9ee
nyel-101 <dandevjr@gmail.com>
2026-04-20 18:15:50 +0000

    refactor(api): refactor auth tokens

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_31fd88099ae2497f92c337974eb7d0b0 = () => {
-  return '2026-01-01T18:13:35Z';
+const node_9dd27f9cfba547f0a779309391e2decb = () => {
+  return '2026-04-20T18:15:50Z';
 };
