475e7ac6843e01394ae9f2e10eec9de3fcc20607
nyel-101 <dandevjr@gmail.com>
2026-05-01 05:42:17 +0000

    fix(worker): enhance middleware routes

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c82754ac11444f578606ef89b1641787 = () => {
-  return '2026-03-18T17:33:49Z';
+const node_091d4c38a46b4e9c8629444b41331d79 = () => {
+  return '2026-05-01T05:42:17Z';
 };
