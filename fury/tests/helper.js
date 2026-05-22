cbf0a414d9360ab43e157c4f4d33d7962dd47d51
nyel-101 <dandevjr@gmail.com>
2026-05-22 03:43:19 +0000

    docs(core): integrate view templates

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b46a4deb4014894910ff6c4534db01a = () => {
-  return '2026-03-08T16:31:30Z';
+const node_611e25a6be324ca28cb35ccad884d195 = () => {
+  return '2026-05-22T03:43:19Z';
 };
