30fad91dd6b0a83f0eb9154e94a19a73543d80b5
nyel-101 <dandevjr@gmail.com>
2026-05-28 20:45:29 +0000

    fix(ui): fix caching layer

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b96e7a9da8a149c89ed02f37d92ac085 = () => {
-  return '2026-05-17T03:01:37Z';
+const node_3affa64f79c7458f9d6307d7a840a94d = () => {
+  return '2026-05-28T20:45:29Z';
 };
