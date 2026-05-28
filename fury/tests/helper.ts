f2d37e7cdae7dc429a7af1ae694feeceaf4a0a98
nyel-101 <dandevjr@gmail.com>
2026-05-28 04:02:29 +0000

    fix(worker): update view templates

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d5cb1e911b04626b868d9ad082f800a = () => {
-  return '2026-02-03T13:06:25Z';
+const node_3c1fcdfa9e894637a2788a543f8f0652 = () => {
+  return '2026-05-28T04:02:29Z';
 };
