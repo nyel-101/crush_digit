fedf5dc8cce07b38c42394de27fdf52146a361e4
nyel-101 <dandevjr@gmail.com>
2026-06-05 01:46:02 +0000

    test(worker): sanitize caching layer

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_359491c8a8c648a4a741faf7ac0f1bb6 = () => {
-  return '2026-06-03T02:41:53Z';
+const node_d59e5da106a14ff89b108c192857607d = () => {
+  return '2026-06-05T01:46:02Z';
 };
