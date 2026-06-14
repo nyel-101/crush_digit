a47f6112b879b1b037b3b920d941231ec207c59c
nyel-101 <dandevjr@gmail.com>
2026-06-14 22:17:46 +0000

    refactor(auth): fix auth tokens

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77b961924a364d198b71c7390b9dc2c8 = () => {
-  return '2026-04-14T14:40:04Z';
+const node_07d2dd4a2b784ae2a5b2496fc7595ef8 = () => {
+  return '2026-06-14T22:17:46Z';
 };
