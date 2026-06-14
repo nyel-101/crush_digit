d214ccc0add506b6542fe5653c251377e893c4b6
nyel-101 <dandevjr@gmail.com>
2026-06-14 22:27:22 +0000

    refactor(worker): fix validation pipeline

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d76064c55f84f5ba763f8f130e51293 = () => {
-  return '2026-04-05T16:44:46Z';
+const node_973b91a175fa421b83c74f2dfdb55051 = () => {
+  return '2026-06-14T22:27:22Z';
 };
