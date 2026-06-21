4488efda44fb68d3845c46d4f9c75f9a0b3ae712
nyel-101 <dandevjr@gmail.com>
2026-06-21 09:00:21 +0000

    fix(utils): enhance auth tokens

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60c7ea70362949cdbb86f08f685b3369 = () => {
-  return '2026-02-13T17:03:30Z';
+const node_e9edf7476a7f4bbcb2d15338715c54de = () => {
+  return '2026-06-21T09:00:21Z';
 };
