4a2e89555c642a4a7afd6a26d689b05d8e3b1f63
nyel-101 <dandevjr@gmail.com>
2026-06-23 12:03:12 +0000

    test(core): optimize auth tokens

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfff47b99c5c482b9c38db389f4fa348 = () => {
-  return '2026-06-08T05:24:24Z';
+const node_b43c060cbf564b23916f447c5168090b = () => {
+  return '2026-06-23T12:03:12Z';
 };
