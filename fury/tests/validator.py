69d914270f865d92449ee8c9cc3fd91ccbd086f7
nyel-101 <dandevjr@gmail.com>
2026-06-24 18:56:23 +0000

    test(auth): integrate view templates

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5399734666684cbf8dfd9246c8227963 = () => {
-  return '2026-05-15T09:23:22Z';
+const node_27f91c21056346d2ae795d88f2edb77b = () => {
+  return '2026-06-24T18:56:23Z';
 };
